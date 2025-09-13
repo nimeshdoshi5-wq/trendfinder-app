83ee558a4a65fb24bb03b0334d7b96eabae797c7
nimeshdoshi5-wq <nimeshdoshi5@gmail.com>
2025-09-13 18:21:53 +0530

    Create app.py
    Added basic Streamlit test app for TrendFinder project

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="TrendFinder Test", layout="wide")
+st.title("🚀 TrendFinder - Test App")
+st.write("अगर यह दिख रहा है तो deployment सफल रहा!")
